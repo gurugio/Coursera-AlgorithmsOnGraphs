7aaf37706c72625d8d94974c7c349926a177a35d
Gioh Kim <gi-oh.kim@cloud.ionos.com>
2020-06-16 19:32:40 +0200

    week1: finish

--- week1_decomposition1/1_reachability/reachability.cpp
+++ week1_decomposition1/1_reachability/reachability.cpp
@@ -1,26 +1,51 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
 using std::vector;
 using std::pair;
 
 
+// 1 - 2
+// 3 - 2
+// 4 - 3
+// 1 - 4
+
+
+void explore(vector<vector<int> > &adj, bool* visited, int current)
+{
+	visited[current] = true;
+	//cout << "VIsiT: " << current << endl;
+	for (const int& i : adj[current]) {
+		if (visited[i] == false)
+			explore(adj, visited, i);
+	}
+}
+
 int reach(vector<vector<int> > &adj, int x, int y) {
-  //write your code here
-  return 0;
+	int ret;
+	bool* visited = new bool[adj.size()];
+	explore(adj, visited, x);
+	ret = visited[y];
+	delete [] visited;
+	return ret;
 }
 
 int main() {
-  size_t n, m;
-  std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  for (size_t i = 0; i < m; i++) {
-    int x, y;
-    std::cin >> x >> y;
-    adj[x - 1].push_back(y - 1);
-    adj[y - 1].push_back(x - 1);
-  }
-  int x, y;
-  std::cin >> x >> y;
-  std::cout << reach(adj, x - 1, y - 1);
+	size_t n, m;
+	// n: number of nodes
+	// m: number of edges
+	std::cin >> n >> m;
+	vector<vector<int> > adj(n, vector<int>());
+	for (size_t i = 0; i < m; i++) {
+		int x, y;
+		std::cin >> x >> y;
+		// no direction
+		adj[x - 1].push_back(y - 1);
+		adj[y - 1].push_back(x - 1);
+	}
+	
+	int x, y;
+	std::cin >> x >> y;
+	std::cout << reach(adj, x - 1, y - 1);
 }
--- week1_decomposition1/2_connected_components/connected_components.cpp
+++ week1_decomposition1/2_connected_components/connected_components.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
 using std::vector;
 using std::pair;
+
+
+void explore(vector<vector<int> > &adj, bool* visited, int current)
+{
+	visited[current] = true;
+	//cout << "VIsiT: " << current << endl;
+	for (const int& i : adj[current]) {
+		if (visited[i] == false)
+			explore(adj, visited, i);
+	}
+}
+
 
 int number_of_components(vector<vector<int> > &adj) {
   int res = 0;
   //write your code here
+
+  bool* visited = new bool[adj.size()];
+
+  for (int i = 0; i < adj.size(); i++)
+	  visited[i] = false;
+
+  for (int i = 0; i < adj.size(); i++) {
+	  if (visited[i] == false) {
+		  res++;
+		  explore(adj, visited, i);
+	  }
+  }
+  
   return res;
 }
 
