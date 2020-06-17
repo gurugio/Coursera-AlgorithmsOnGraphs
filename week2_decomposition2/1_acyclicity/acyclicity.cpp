ad1aff2bcc851008c1f3f9ff7aa93e8dcf8e13c0
Gioh Kim <gi-oh.kim@cloud.ionos.com>
2020-06-17 22:24:38 +0200

    wee2: finish acyclicity

--- week2_decomposition2/1_acyclicity/acyclicity.cpp
+++ week2_decomposition2/1_acyclicity/acyclicity.cpp
@@ -1,12 +1,51 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
 
+using namespace std;
 using std::vector;
 using std::pair;
 
+
+int explore_acyclic(vector<vector<int> >& adj, int* visited_color, int current)
+{
+	int ret = 1;
+	visited_color[current] = 1;
+	cout << "visit: " << (current+1) << endl;
+
+	for (const int& i: adj[current]) {
+		cout << "check: " << (current+1) << "->" << (i+1) << endl;
+		if (visited_color[i] == 0) {
+			if (explore_acyclic(adj, visited_color, i) == 0)
+				return 0; // found cycle
+		} else if (visited_color[i] == 1) { // found back-edge
+			cout << "back-edge: " << (current+1) << "->" << (i+1) << endl;
+			return 0; // found cycle
+		}
+		//assert(visited_color[i] != 2);
+	}
+	visited_color[current] = 2; // black, finished
+	cout << "finish: " << (current+1) << endl;
+	return 1; // acyclic
+}
+
 int acyclic(vector<vector<int> > &adj) {
-  //write your code here
-  return 0;
+	// 0: white, not visited
+	// 1: gray, discovered
+	// 2: black, finished
+	int* visited_color = new int[adj.size()];
+
+	for (int i = 0; i < adj.size(); i++)
+		visited_color[i] = 0;
+
+	for (int i = 0; i < adj.size(); i++) {
+		if (visited_color[i] == 0) {
+			if (explore_acyclic(adj, visited_color, i) == 0)
+				return 0; // found cycle
+		}
+	}
+	
+	return 1; // acyclic
 }
 
 int main() {
