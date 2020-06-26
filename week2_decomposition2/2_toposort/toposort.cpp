c534364e6f7887e72731be411cfbc9c5c7b2a60d
Gioh Kim <gi-oh.kim@cloud.ionos.com>
2020-06-26 13:46:33 +0200

    toposort.cpp: remove 1st solution because it is wrong

--- week2_decomposition2/2_toposort/toposort.cpp
+++ week2_decomposition2/2_toposort/toposort.cpp
@@ -7,63 +7,7 @@ using std::pair;
 using namespace std;
 
 
-#if 0
-//
-// first solution(slow): run DFS for all nodes
-//
-
-void dfs(vector<vector<int> > &adj, vector<int> &used, vector<int> &order, int x) {
-	//write your code here
-	used[x] = 1;
-	cout << "visit: " << x << endl;
-
-	// if no out-edge, remove it from adj and append it to order
-	if (adj[x].empty()) {
-		cout << "add order: " << x << endl;
-		order.push_back(x);
-
-		for (int i = 0; i < adj.size(); i++) {
-			if (!adj[i].empty() && adj[i][0] == x) {
-				adj[i].pop_back();
-			}
-		}
-	} else if (used[adj[x][0]] == 0) {
-			dfs(adj, used, order, adj[x][0]);
-	}
-}     
-
-vector<int> toposort(vector<vector<int> > adj) {
-	vector<int> used(adj.size(), 0);
-	vector<int> order;
-	//write your code here
-
-	for (int i = 0; i < adj.size(); i++) {
-		cout << "start dfs: " << i << endl;
-		if (!adj[i].empty())
-			dfs(adj, used, order, i);
-
-		// clear visited flag
-		for (int j = 0; j < used.size(); j++)
-			used[j] = 0;
-	}
-
-	// all nodes are disconnected now.
-	// add remained nodes to order
-	for (int i = 0; i < adj.size(); i++) {
-		bool found = false;
-		for (int j = 0; j < order.size(); j++) {
-			if (i == order[j])
-				found = true;
-		}
-		if (!found)
-			order.push_back(i);
-	}	
-	
-	return order;
-}
-#endif
-
-// 2nd fast solution: run dfs once and add visited nodes reversely
+// run dfs once and add visited nodes reversely
 void dfs(vector<vector<int> > &adj, vector<int> &used, vector<int> &order, int x) {
 	//write your code here
 
